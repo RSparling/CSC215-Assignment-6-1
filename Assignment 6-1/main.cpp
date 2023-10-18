30f1c7f83b32d44760fb92f956ecf249903c1dca
RSparling <133298282+RSparling@users.noreply.github.com>
2023-10-18 14:10:55 -0700

    Update main.cpp

--- Assignment 6-1/main.cpp
+++ Assignment 6-1/main.cpp
@@ -1,20 +1,9 @@
-/*
-Russell Sparling
-Assignment 6.1
-Professor: Tony Hinton
-Due: September 24. 2024
-
-Disclaimer to Professor:
-This project is based on the earlier 3.1 assignment though the primary thing that was reused was the 3 player resources
-and the text for the scenarios. Other than that the way this is made makes it much more extensible and includes a builder pattern
-for the creation of each encounter which have been named Scenes.
-*/
-
 /*TODO for continued work:
-- Make a json serializer/deserializer
-- Write a progam to create encounters in a much easier manner by serializing them into a form this can deserialize
+- Make a JSON serializer/deserializer
+- Write a program to create encounters in a much easier manner by serializing them into a form that can deserialize
 - Refine codebase to reduce coupling
-- Clean up Game.cpp to better comparmentalize*/
+- Clean up Game.cpp to better compartmentalize*/
+
 #include<iostream>
 #include "Game.h"
 
